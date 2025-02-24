d1d9a238a2822dece120ad45ffd2afcc6748985d
Gulammustafa03 <gulammustafa0431@gmail.com>
2025-02-24 09:55:51 +0530

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
- <script src="https://unpkg.com/typed.js@2.1.0/dist/typed.umd.js"></script>
-    <script>
-        var typed = new Typed('#element', {
-            strings: ['Web Developer', 'Graphic Designer', 'Web Designer'],
-            typeSpeed: 50,
-        });
-    </script>
